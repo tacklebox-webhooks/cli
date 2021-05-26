674a5a147dec26bf332e3a16ec01cb5bffe54532
ArmandoMota <armandomotagt@gmail.com>
2021-05-26 11:41:17 -0700

    Finalized showing ApiKey in output

--- src/tasks/populateUiEnv.js
+++ src/tasks/populateUiEnv.js
@@ -10,9 +10,8 @@ const client = new APIGatewayClient();
 const populateUiEnv = () => {
   return new Observable((observer) => {
     observer.next("Extracting environment data");
-    const { apiUrl, apiKeyId } = extractApiData();
-
-    writeApiDataToEnv(apiUrl, apiKeyId, observer);
+    const apiData = extractApiData();
+    writeApiDataToEnv(apiData, observer);
   });
 };
 
@@ -22,24 +21,32 @@ const extractApiData = () => {
   return json.Tacklebox;
 };
 
-const writeApiDataToEnv = (apiUrl, apiKeyId, observer) => {
+const writeApiDataToEnv = (apiData, observer) => {
   observer.next("Translating environment data");
+  const { apiUrl, apiKeyId } = apiData;
   const command = new GetApiKeyCommand({
     apiKey: apiKeyId,
     includeValue: true,
   });
   client.send(command, (err, data) => {
-    const apiKey = data.value;
     observer.next("Injecting environment variables");
+    const apiKey = data.value;
+    updateOutputFile(apiData, apiKey);
     writeEnvFile(apiUrl, apiKey);
     observer.complete();
   });
 };
 
+const updateOutputFile = (apiData, apiKey) => {
+  const filepath = "src/infrastructure/outputs.json";
+  const updatedData = { ...apiData, apiKey };
+  const fileContent = JSON.stringify({ Tacklebox: updatedData }, null, 2);
+  fs.writeFileSync(filepath, fileContent);
+};
+
 const writeEnvFile = (apiUrl, apiKey) => {
   const filepath = "src/ui/client/.env";
   const fileContent = `REACT_APP_HOST=${apiUrl}\nREACT_APP_API_KEY=${apiKey}`;
-
   fs.writeFile(filepath, fileContent, (err) => {
     if (err) throw err;
   });
